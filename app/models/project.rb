ed91c63f370814bde6665eec8b44010000595ff9
Kazuto Kusama <jacopen@gmail.com>
2018-12-02 23:35:39 +0900

    Edit validation rules

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,10 +18,10 @@ end
 class Project < ApplicationRecord
   include ActiveModel::Validations
   validates_with GitHubUserValidator
-  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-z0-9\-]+\z/}, length: { maximum: 50 }
+  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-z0-9\-]+\z/}, length: { maximum: 30 }
   validates :github_id, uniqueness: true, presence: true, length: { maximum: 30 } #FIXME: need to check validation rule about github id
   validates :twitter_id, format: { with: /\A[a-zA-Z0-9\_]+\z/}, length: { maximum: 15 }
-  validates :comment, length: { maximum: 500 }
+  validates :comment, length: { maximum: 100 }
 
   before_create :provision
   before_destroy :cleanup
